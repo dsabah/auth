6be40701c7456e444fa1f936f5645db5b0eca8d2
Sabah Dan <checksabah@gmail.com>
2019-01-30 14:39:02 +0000

    First Commit

---
+++ hash_jwt.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcrypt');
+const {MD5} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+const id = '1000';
+const secret = 'supersecret'
+
+const receivedToken ='eyJhbGciOiJIUzI1NiJ9.MTAwMA.L9PmEqLlZjettygguzj25agunJu6NkvVtG9RFRBnK2Y';
+
+const token =jwt.sign(id,secret)
+const decodeToken = jwt.verify(receivedToken,secret)
+
+console.log(decodeToken);
